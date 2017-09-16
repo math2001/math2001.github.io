29a2c121bd5bb3963915f05a495212f5f1e914e9
Mathieu PATUREL <australie.p@gmail.com>
2017-09-16 16:15:26 +1000

    :construction: display infos @ deployer

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,17 @@ set -o errexit # abort if a command fails
 
 VERSION="0.1.0"
 BRANCH=$(hugo config | grep publishbranch | egrep '"[^!\^:\\ ]+"' -o | tr -d '"')
-CURRENT_BRANCH=$(git rev-parse --abbrev-red HEAD)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Current branch: $CURRENT_BRANCH"
+echo "Target branch: $BRANCH"
+
+echo -n "Keep going (y/n): "
+read line
+if [[ $line != 'y' ]]; then
+    echo "Abort"
+    exit 1
+fi
 
 if [[ "$(git branch --list $BRANCH)" == '' ]]; then
     # create branch
