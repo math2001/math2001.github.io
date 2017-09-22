e3b7da1dd3b2879a5f7c8cbc7f5caaea17e2256e
Mathieu PATUREL <australie.p@gmail.com>
2017-09-22 16:49:11 +1000

    :construction: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,22 +9,30 @@ fi
 THEMES_DIR=$(hugo config | grep "themesdir" | egrep '".+"' -o)
 THEMES_DIR=${THEMES_DIR:1:${#THEMES_DIR}-2}
 
-TARGET_BRANCH=$(hugo config | grep "pushblishbranch" | egrep '".+"' -o)
+TARGET_BRANCH=$(hugo config | grep "publishbranch" | egrep '".+"' -o)
 TARGET_BRANCH=${TARGET_BRANCH:1:${#TARGET_BRANCH}-2}
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 CURRENT_COMMIT=$(git rev-parse --short HEAD)
 
+echo "Themes directory: '$THEMES_DIR'"
+echo "Target branch: '$TARGET_BRANCH'"
+echo "Current branch: '$CURRENT_BRANCH'"
+
+echo -n "Continue? (y/N) "
+read line
+if [[ $line != 'y' ]]; then
+    echo "Abort"
+    exit 1
+fi
+
 TEMP=$(mktemp -d)
 hugo -d "$TEMP"
 
 git checkout "$TARGET_BRANCH"
 
-find -not -path "*.git*" -not -path "*$THEMES_DIR*" -type f
-
-echo 'DEV: exit'
-exit 1
+find -not -path "*.git*" -not -path "*$THEMES_DIR*" -type f -delete
 
 mv "$TEMP/*" .
 
