23db23532d7aab6c2967a41ae4b609fd09b47c52
Mathieu PATUREL <australie.p@gmail.com>
2017-09-27 15:21:15 +1000

    :bug: in server.sh

--- server.sh
+++ server.sh
@@ -1,11 +1,8 @@
-if [[ $@ == *--kill* ]]; then
-    kill $(jobs -p)
-    exit 0
-fi
-
 if [[ $@ == *--drafts* ]]; then
     hugo server --disableLiveReload --buildDrafts &
 else
     hugo server --disableLiveReload &
 fi
+export HUGO_PID=$!
 browser-sync start --proxy "localhost:1313" --files "**/static" --staticFiles "**/static" --no-notify &
+export BROWSER_SYNC_PID=$!
