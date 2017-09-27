4d282c75bf2ec470be3514950467c44082afd0ef
Mathieu PATUREL <australie.p@gmail.com>
2017-09-27 10:58:44 +1000

    :sparkles: add server.sh

---
+++ server.sh
@@ -0,0 +1,11 @@
+if [[ $@ == *--kill* ]]; then
+    kill $(jobs -p)
+    exit 0
+fi
+
+if [[ $@ == *--drafts* ]]; then
+    hugo server --disableLiveReload --buildDrafts &
+else
+    hugo server --disableLiveReload &
+fi
+browser-sync --start --proxy "localhost:1313" --files "**/static" --staticFiles "**/static" --no-notify &
