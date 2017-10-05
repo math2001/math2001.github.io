0a90d859e349eb1d0675aa39397da54a9406566c
Mathieu PATUREL <australie.p@gmail.com>
2017-10-06 08:32:14 +1100

    :sparkles: server.sh working

--- Makefile
+++
@@ -1,5 +0,0 @@
-.PHONY: server
-
-server: 
-	browser-sync start --proxy localhost:1313 --files "static" --staticFiles "static" --no-notify --no-ui &
-	hugo server --disableLiveReload -D
--- server.sh
+++ server.sh
@@ -1,8 +1,3 @@
-if [[ $@ == *--drafts* ]]; then
-    hugo server --disableLiveReload --buildDrafts &
-else
-    hugo server --disableLiveReload &
-fi
-export HUGO_PID=$!
-browser-sync start --proxy "localhost:1313" --files "**/static" --staticFiles "**/static" --no-notify &
-export BROWSER_SYNC_PID=$!
+hugo server --disableLiveReload &
+cd themes/fasty
+browser-sync start --proxy 'localhost:1313' --files 'static' --ss 'static'
