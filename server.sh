fa01a5f7ebeb7f8812a7e9c651ce3536ce0dbd03
Mathieu PATUREL <australie.p@gmail.com>
2017-10-06 16:42:18 +1100

    :bug: watch every themes files @ server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 hugo server --disableLiveReload &
 cd themes/fasty
-browser-sync start --proxy 'localhost:1313' --files 'static' --ss 'static'
+browser-sync start --proxy 'localhost:1313' --files '**/*' --ss 'static' --no-notify --no-ui --no-open
